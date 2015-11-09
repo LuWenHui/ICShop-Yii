2bdb82b18c3a050319328e0fc2a8d118e1b64535
suxiaolin <suxiaolin@mail.com>
2015-11-09 11:59:23 +0800

    完成创建后台国际化文件

--- backend/messages/zh-CN/app.php
+++ backend/messages/zh-CN/app.php
@@ -5,4 +5,9 @@ return [
     'Password' => '密码',
     'Captcha' => '验证码',
     'Incorrect username or password.' => '用户名和密码组合错误。',
+    
+    // orm中的字段
+    'Status' => '状态',
+    'CreatedTime' => '创建时间',
+    'UpdatedTime' => '修改时间',
 ];
