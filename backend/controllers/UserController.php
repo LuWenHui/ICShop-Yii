46cea5fe57dd02c835fc8337bb9f645bb4444c56
suxiaolin <suxiaolin@mail.com>
2015-11-12 15:40:04 +0800

    生成多种颜色的验证码

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 
 use Yii;
 use yii\web\Controller;
-use yii\captcha\CaptchaAction;
+use common\components\actions\CaptchaAction;
 use yii\filters\AccessControl;
 use backend\models\AdminUserLoginForm;
 
