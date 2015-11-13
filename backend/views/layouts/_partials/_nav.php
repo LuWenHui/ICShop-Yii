e1a5aeb0ce2b7688a87be746b271bd7ec8f4494c
suxiaolin <suxiaolin@mail.com>
2015-11-13 19:10:40 +0800

    使用组件替换硬编码左侧菜单

--- backend/views/layouts/_partials/_nav.php
+++ backend/views/layouts/_partials/_nav.php
@@ -1,94 +1,80 @@
 <?php
 
 use yii\helpers\Url;
-
+use backend\widgets\Nav;
+use yii\helpers\StringHelper;
 ?>
 <!-- nav -->
 <nav class="nav-primary hidden-xs">
     <div class="text-muted text-sm hidden-nav-xs padder m-t-sm m-b-sm">目录</div>
-    <ul class="nav nav-main" data-ride="collapse">
-        <li >
-            <a href="<?= Url::to(['dashboard/index']) ?>" class="auto">
-            <i class="i i-statistics icon">
-            </i>
-            <span class="font-bold">统计分析</span>
-            </a>
-        </li>
-        <li >
-            <a href="#" class="auto">
-            <span class="pull-right text-muted">
-            <i class="i i-circle-sm-o text"></i>
-            <i class="i i-circle-sm text-active"></i>
-            </span>
-            <i class="i i-stack icon">
-            </i>
-            <span class="font-bold">产品管理</span>
-            </a>
-            <ul class="nav dk">
-                <li >
-                    <a href="<?= Url::to(['product/index']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>产品列表</span>
-                    </a>
-                </li>
-                <li >
-                    <a href="<?= Url::to(['product/create']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>添加产品</span>
-                    </a>
-                </li>
-                <li >
-                    <a href="<?= Url::to(['product-category/index']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>分类列表</span>
-                    </a>
-                </li>
-                <li >
-                    <a href="<?= Url::to(['product-category/create']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>添加分类</span>
-                    </a>
-                </li>
-                <li >
-                    <a href="<?= Url::to(['product-attribute-category/index']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>属性分类</span>
-                    </a>
-                </li>
-                <li >
-                    <a href="<?= Url::to(['product-attribute/index']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>属性列表</span>
-                    </a>
-                </li>
-                <li >
-                    <a href="<?= Url::to(['product-attribute/create']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>添加属性</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li >
-            <a href="#" class="auto">
-            <span class="pull-right text-muted">
-            <i class="i i-circle-sm-o text"></i>
-            <i class="i i-circle-sm text-active"></i>
-            </span>
-            <i class="i i-meter icon">
-            </i>
-            <span class="font-bold">订单管理</span>
-            </a>
-            <ul class="nav dk">
-                <li>
-                    <a href="<?= Url::to(['order/index']) ?>" class="auto">
-                    <i class="i i-dot"></i>
-                    <span>订单列表</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
+    <?= Nav::widget([
+        'items' => [
+            [
+                'label' => '统计分析',
+                'url' => ['dashboard/index'],
+                'icon' => 'i i-statistics icon',
+                'rightIcon' => 'i i-circle-sm',
+                'font' => 'font-bold',
+            ],
+            [
+                'label' => '产品管理',
+                'icon' => 'i i-statistics icon',
+                'rightIcon' => 'i i-circle-sm',
+                'font' => 'font-bold',
+                'items' => [
+                     [
+                        'label' => '产品列表',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product/index'],
+                    ],
+                    [
+                        'label' => '添加产品',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product/create'],
+                    ],
+                    [
+                        'label' => '分类列表',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product-category/index'],
+                    ],
+                    [
+                        'label' => '添加列表',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product-category/create'],
+                    ],
+                    [
+                        'label' => '属性分类',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product-attribute-category/index'],
+                    ],
+                    [
+                        'label' => '属性列表',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product-attribute/index'],
+                    ],
+                    [
+                        'label' => '添加属性',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['product-attribute/create'],
+                    ],
+                ],
+            ],
+            [
+                'label' => '订单管理',
+                'icon' => 'i i-meter icon',
+                'rightIcon' => 'i i-circle-sm',
+                'font' => 'font-bold',
+                'items' => [
+                     [
+                        'label' => '订单列表',
+                        'icon' => 'i i-dot icon',
+                        'url' => ['order/index'],
+                    ],
+                ],
+            ],
+        ],
+    ]) ?>
+    
     <div class="line dk hidden-nav-xs"></div>
     <div class="text-muted text-xs hidden-nav-xs padder m-t-sm m-b-sm">快捷访问</div>
     <ul class="nav">
@@ -103,7 +89,7 @@ use yii\helpers\Url;
             <i class="i i-circle-sm text-success-dk"></i>
             <span>订单列表</span>
             </a>
-        </li
+        </li>
     </ul>
 </nav>
 <!-- / nav -->
