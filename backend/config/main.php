dae171e5dec7c8440a464deb94f3425755054bb0
suxiaolin <dear.lin@live.com>
2015-11-27 17:14:50 +0800

    stagging commit

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,12 +13,19 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'defaultRoute' => 'dashboard/index',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ],
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\AdminUser',
             'enableAutoLogin' => true,
             'loginUrl' => ['user/login'],
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\DbManager'
         ],
         'assetManager' => [
             'bundles' => [
@@ -38,4 +45,18 @@ return [
         ],
     ],
     'params' => $params,
+    
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],
 ];
