85b994741b72e387529fe8ff91e1980b2b924840
suxiaolin <dear.lin@live.com>
2015-12-02 17:04:35 +0800

    add token to email message

--- backend/controllers/MailController.php
+++ backend/controllers/MailController.php
@@ -9,13 +9,14 @@ use yii\helpers\Url;
 class MailController extends Controller
 {
 	public function actionSend() {
-		$activeAccountLink = Url::to('/', true);
+		$activeAccountLink = Url::to(['/', 'token' => Yii::$app->security->generateRandomString()], true);
 		//\yii\helpers\VarDumper::dump($activeAccountLink);die;
-		Yii::$app->mailer->compose(['html' => 'active-account-html', 'text' => 'active-account-text'], ['activeAccountLink' => $activeAccountLink])
+		$flag = Yii::$app->mailer->compose(['html' => 'active-account-html', 'text' => 'active-account-text'], ['activeAccountLink' => $activeAccountLink])
 			->setFrom('postmaster@sandbox82dfe3fbf0614ea28b17eadd5b0818f8.mailgun.org')
 			->setTo('dear.lin@live.com')
 			->setSubject('激活账户代码')
 			->send();
+		var_dump($flag);
 		die;
 	}
 }
