73f0639e138373405d70f958ff08ed2a51e11129
suxiaolin <dear.lin@live.com>
2015-12-03 10:47:31 +0800

    更新配置文件到环境文件中

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,20 +1,28 @@
 <?php
 return [
     'components' => [
+        'assetManager' => [
+            'linkAssets' => true,
+        ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=itcast-shop',
             'username' => 'root',
-            'password' => '',
+            'password' => 'hahaha',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            //'useFileTransport' => 'true',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.mailgun.org',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => 'postmaster@sandbox82dfe3fbf0614ea28b17eadd5b0818f8.mailgun.org',
+                'password' => 'aafbf3bb5ca03fc8261490f081675f98',
+                'port' => '587', // Port 25 is a very common port too
+                'encryption' => 'tls', // It is often used, check your provider or mail server specs
+            ],
         ],
     ],
 ];
